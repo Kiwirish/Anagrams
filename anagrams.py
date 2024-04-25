5b6cbbaffbc6fbec348ad1fa6deb1ed49e225a27
rulja771 <rulja771@student.otago.ac.nz>
2024-04-25 15:00:34 +1200

    merge

--- anagrams.py
+++ anagrams.py
@@ -33,12 +33,23 @@ def main():
 
 def findAnagram(word, dic):
 
-    length = len(word)
+    wordlength = len(word)
     letters = countLetters(word)
+    dicLargestWord = max(dic.keys())
+
+    if (wordlength > dicLargestWord):
+        length = dicLargestWord
+    else:
+        length = wordlength
 
     for num in range(length, 1, -1):
-        dic[num]
-    return
+        dicList = dic[num]
+        for dicWord in dicList:
+            dicLetters = countLetters(dicWord)
+            if letters == dicLetters:
+                return dicWord
+            
+                
 
 def countLetters(word):
     # Count occurrences of each character in the word
@@ -46,5 +57,17 @@ def countLetters(word):
     for char in word:
             count[char] = count.get(char, 0) + 1
     return count
+
+def splitWords(letters, dicLetters):
+    leftover_letters = letters.copy() 
+    for char, count in dicLetters.items():
+        if char not in leftover_letters or leftover_letters[char] < count:
+            return None  # Invalid dicLetters, return None
+        leftover_letters[char] -= count
+    return leftover_letters
+
+    # for char in dicLetters:
+    #     if char in letters:
+    #         letters[char] = letters.get(char) - 1
 
 main()
